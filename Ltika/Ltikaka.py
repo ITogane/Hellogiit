4289563ea602829eec4efc3f8aa5d6fdea293f77
raspberrypiItogane <itsukiogane@gmai.com>
2018-08-10 15:58:26 +0000

    add Servo files

--- Ltika/Ltikaka.py
+++ Ltika/Ltikaka.py
@@ -2,11 +2,12 @@ import RPi.GPIO as GPIO
 import time
 
 led_pin = 2
-time_sleep = 0.02
+time_sleep = 0.2
+what_times = 10
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(led_pin, GPIO.OUT)
 
-while True:
+for i in range(what_times):
     GPIO.output(led_pin,1)
     time.sleep(time_sleep)
     GPIO.output(led_pin,0)
